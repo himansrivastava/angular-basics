467df6a7dd3a3c09ed0515708b13150ad2d4eac1
Himanshu Srivastava <h.dataconsultancy@gmail.com>
2022-08-11 13:27:07 +0530

    add inline template and css

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  // selector: 'app-test',
+  // selector: '.app-test',
+  selector: '[app-test]',
+  // templateUrl: './test.component.html',
+  // we can use template instead of templateUrl to use inline html
+  template:
+    `
+    <div>
+      <h1>Hello World</h1>
+    </div>
+    `,
+  // styleUrls: ['./test.component.css']
+  // we can use style instead of styleUrl to use inline css
+  styles: [
+    `
+    h1 {
+      color: red;
+    }
+    `
+  ]
 })
 export class TestComponent implements OnInit {
 
